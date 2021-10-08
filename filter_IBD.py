1b34da86ef05c70c7b42f643d4ba903cb30715ad
ruthkepstein <ruthkepstein@gmail.com>
2021-10-08 12:06:59 -0400

    updated!!

--- filter_IBD.py
+++ filter_IBD.py
@@ -2,6 +2,6 @@
 file = open("Mo17_B73.paf", "r")
 for it in file:
     data = it.split("\t")
-    if int(data[9]) >= 10000 & float(data[9])/float(data[10])<0.2:
-        print(data[5,7:9])
+    if (int(data[9]) >= 10000 & (float(data[9])/float(data[10])<0.2) & (data[0] == data[5])):
+        print(data[5]+"\t"+data[7]+"\t"+data[8]+"\t")
 file.close()
