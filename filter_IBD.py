58c52b804520b765c6df48ffc649ffbe1f8bbd5d
ruthkepstein <ruthkepstein@gmail.com>
2021-10-08 09:53:57 -0400

    First commit

---
+++ filter_IBD.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+file = open("Mo17_B73.paf", "r")
+for it in file:
+    data = it.split("\t")
+    if int(data[9]) >= 10000 & float(data[9])/float(data[10])<0.2:
+        print(data[5,7:9])
